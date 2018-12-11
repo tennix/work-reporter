8187c8b8b360506f23325576ab7359b0a23a1ec8
winkyao <golangwink@gmail.com>
2018-12-11 10:43:03 +0800

    jira order by assignee

--- daily.go
+++ daily.go
@@ -71,18 +71,18 @@ func runDailyCommandFunc(cmd *cobra.Command, args []string) {
 
 
 	members := strings.Join(allMemberEmals, ",")
-	dailyIssues := queryJiraIssues(fmt.Sprintf(`assignee in (%v)  AND updated >= -1d ORDER BY updated`, members))
+	dailyIssues := queryJiraIssues(fmt.Sprintf(`assignee in (%v)  AND updated >= -1d ORDER BY assignee`, members))
 	formatSectionForSlackOutput(&buf, "Team JIRA Issue", "Updated in last 24 hours")
 	formatJiraIssuesForSlackOutput(&buf, dailyIssues)
 	buf.WriteString("\n")
 
 	processingStatus := `"Job Closed", 完成, TODO, "To Do", DUPLICATED, Blocked, Closed, "WON'T FIX", Paused, Resolved`
-	dueDateIssues := queryJiraIssues(fmt.Sprintf(`status not in (%v) AND assignee in (%v) and duedate <= 2d  ORDER BY updated`, processingStatus, members))
+	dueDateIssues := queryJiraIssues(fmt.Sprintf(`status not in (%v) AND assignee in (%v) and duedate <= 2d  ORDER BY assignee`, processingStatus, members))
 	formatSectionForSlackOutput(&buf, "Getting To Due Date JIRA Issue", "The due date will be less than 2 day")
 	formatJiraIssuesForSlackOutput(&buf, dueDateIssues)
 	buf.WriteString("\n")
 
-	processingIssues := queryJiraIssues(fmt.Sprintf(`status not in (%v) AND assignee in (%v) ORDER BY updated`, processingStatus, members))
+	processingIssues := queryJiraIssues(fmt.Sprintf(`status not in (%v) AND assignee in (%v) ORDER BY assignee`, processingStatus, members))
 	formatSectionForSlackOutput(&buf, "JIRA Issue Without Due Date", "Please add due date to processing JIRA issues")
 	formatJiraIssuesForSlackOutput(&buf, findOutIssuesWithoutDueDate(processingIssues))
 	buf.WriteString("\n")
