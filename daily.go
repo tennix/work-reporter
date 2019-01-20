2e5b4a74ec7a6e801675a87c202e1bcd2f1b7870
winkyao <golangwink@gmail.com>
2019-01-20 14:40:38 +0800

    daily: remove won't fix state

--- daily.go
+++ daily.go
@@ -76,7 +76,8 @@ func runDailyCommandFunc(cmd *cobra.Command, args []string) {
 	formatJiraIssuesForSlackOutput(&buf, dailyIssues)
 	buf.WriteString("\n")
 
-	nonProcessStatus := `"Job Closed", 完成, TODO, "To Do", DUPLICATED, Blocked, Closed, "WON'T FIX", Paused, Resolved, "CAN'T REPRODUCE", Cancelled`
+	// TODO: make the filter syntax configurable in the config file.
+	nonProcessStatus := `"Job Closed", 完成, TODO, "To Do", DUPLICATED, Blocked, Closed, Paused, Resolved, "CAN'T REPRODUCE", Cancelled`
 	dueDateIssues := queryJiraIssues(fmt.Sprintf(`status not in (%v) AND assignee in (%v) and duedate <= 2d  ORDER BY assignee`, nonProcessStatus, members))
 	formatSectionForSlackOutput(&buf, "Getting To Due Date JIRA Issue", "The due date will be less than 2 day")
 	formatJiraIssuesForSlackOutput(&buf, dueDateIssues)
